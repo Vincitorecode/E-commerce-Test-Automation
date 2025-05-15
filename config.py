6345673752c4506bfe77b724df330ef5661cf3ef
Victor <vincitore.code@gmail.com>
2025-05-15 01:24:04 -0600

    webdriver-manager auto

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 class Config:
     BASE_URL = "https://www.saucedemo.com"  # URL de la tienda ficticia
-    DRIVER_PATH = r"C:\webdriver\chromedriver.exe"  # Ruta de tu chromedriver
 
     @staticmethod
     def get_driver():
-        """Obtiene el driver de Chrome configurado"""
-        service = Service(Config.DRIVER_PATH)
-        options = webdriver.ChromeOptions()
+        """Obtiene el driver de Chrome usando webdriver-manager"""
+        options = Options()
         options.add_argument("--start-maximized")  # Maximiza la ventana del navegador
+        # Puedes agregar más opciones aquí si necesitas
+        service = Service(ChromeDriverManager().install())
         return webdriver.Chrome(service=service, options=options)
+
 
